218978cb25c731ebd95f13887beef7cd9106500b
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-01-19 14:23:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9104 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,12 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline # 입력 시간 줄이기
 N = int(input())
-result = [0]*10001
-for i in range(1, N+1):
+result = [0]*10001 # 최대 입력 수만큼 0 리스트 생성
+for i in range(1, N+1): # 입력된 수(중복가능)의 인덱스에 1 추가
     num = int(input())
     result[num] += 1
 
-for i in range(1, 10001):
+for i in range(1, 10001): # 다시 전체 돌면서 0이 아니면
     if result[i] != 0:
-        for j in range(result[i]):
+        for j in range(result[i]): # 카운트된 수만큼 그 숫자 출력(계수정렬)
             print(i)
