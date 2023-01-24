6bd198a069a0c5d0630f8706c0c2919eb24ab98d
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-01-24 16:29:27 +0900

    [Silver V] Title: 나이순 정렬, Time: 232 ms, Memory: 52420 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,7 +1,9 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline())
 student_list = []
 for test_case in range(N):
-    student = tuple(input().split())
+    student = tuple(sys.stdin.readline().split())
     student_list.append(student)
 
 student_list = sorted(student_list, key = lambda x: int(x[0]))
