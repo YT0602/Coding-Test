e797598860be9a41674b04e7cfc7d292e79607d9
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-01-31 23:30:12 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 while True:
-    words = list(input())
+    words = list(input().rstrip())
     stk = []
-    if words == ['.']:
+    if words == ['.']:  # . 입력되면 종료
         break
     for i in words:
-        if '(' in i:
+        if '(' in i:  # ( or [ 면 스택에 추가
             stk.append('(')
         if '[' in i:
             stk.append('[')
-        if ')' in i:
+        if ')' in i:  # 원소가 ), ] 일때 스택이 비어있거나 pop한 원소가 [, ( 면 해당 원소 추가하고 종료
             if len(stk) == 0 or stk.pop() == '[':
                 stk.append(')')
                 break
@@ -16,7 +19,7 @@ while True:
             if len(stk) == 0 or stk.pop() == '(':
                 stk.append(']')
                 break
-    if len(stk) == 0:
+    if len(stk) == 0:  # 스택이 비어있으면 yes 아니면 no
         print('yes')
     else:
         print('no')
