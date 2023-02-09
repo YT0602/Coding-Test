b1f24e34311c3b383897a589000608c5f25291fc
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-09 13:48:53 +0900

    [Bronze I] Title: 색종이, Time: 436 ms, Memory: 121596 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -2,17 +2,21 @@ import sys
 
 input = sys.stdin.readline
 N = int(input())
-arr = [[0]*1001 for _ in range(1001)]
-num = 1
+arr = [[0] * 1001 for _ in range(1001)]
 cnt = [0] * (N + 1)
+num = N
+points = []
 for i in range(N):
-    x, y, w, h = map(int, input().split())
-    for j in range(y, y+h):
-        for k in range(x, x+w):
-            arr[j][k] = num
-    num += 1
-for i in arr:
-    for j in range(1, num):
-        cnt[j] += i.count(j)
-for i in range(1, N+1):
+    square = list(map(int, input().split()))
+    points.append(square)
+    # x, y, w, h
+for i in range(len(points) - 1, -1, -1):
+    for j in range(points[i][1], points[i][1] + points[i][3]):
+        for k in range(points[i][0], points[i][0] + points[i][2]):
+            if arr[j][k] == 0:
+                arr[j][k] = num
+                cnt[num] += 1
+    num -= 1
+
+for i in range(1, N + 1):
     print(cnt[i])
