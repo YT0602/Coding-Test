281dc2da3a942ab8bb144bd4398b5cca69447106
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-11 19:16:55 +0900

    [D2] Title: 스도쿠 검증, Time: 142 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,41 +1,26 @@
-def ch_row(arr):
-    for row in range(9):
-        check = set()
-        for col in range(9):
-            check.add(arr[row][col])
-        if len(check) != 9:
-            return
-    return True
-
-
-def ch_col(arr):
-    for col in range(9):
-        check = set()
-        for row in range(9):
-            check.add(arr[row][col])
-        if len(check) != 9:
-            return
-    return True
-
-def square(arr):
-    for j in range(3):
-        for k in range(3):
-            check = set()
-            for x in range(3):
-                for y in range(3):
-                    check.add(arr[x + j*3][y + k*3])
-            if len(check) != 9:
-                return
-    return True
+def check(arr):
+    # 행 판별
+    for row in arr:
+        # 중복된 수가 있으면 개수가 9가 아니므로 검색 중단
+        if len(set(row)) != 9:
+            return 0
+    # 열 판별
+    arr_t = list(zip(*arr))
+    for col in arr_t:
+        if len(set(col)) != 9:
+            return 0
+    # 3x3 판별
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):
+            lst = arr[i][j:j+3] + arr[i+1][j:j+3] + arr[i+2][j:j+3]
+            if len(set(lst)) != 9:
+                return 0
+    return 1
 
 
 T= int(input())
 for tc in range(1, T+1):
-    sudoku = []
-    for i in range(9):
-        sudoku.append(list(map(int, input().split())))
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
 
-    if ch_col(sudoku) and ch_row(sudoku) and square(sudoku):
-        print(f'#{tc} 1')
-    else:
-        print(f'#{tc} 0')
+    ans = check(sudoku)
+    print(f'#{tc} {ans}')
