d1f39faf3ca70e9cc241d9c3e35cc2608358ac4b
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-14 17:17:31 +0900

    [D4] Title: 쇠막대기 자르기, Time: 287 ms, Memory: 89,028 KB -BaekjoonHub

---
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -0,0 +1,20 @@
+T = int(input())
+for tc in range(1, T+1):
+    lazer = list(input())
+    stack = []
+    # 잘린 조각
+    seg = 0
+    for i in range(len(lazer)):
+        if lazer[i] == '(':
+            if lazer[i+1] == ')':
+                seg += len(stack)
+            else:
+                seg += 1
+                stack.append('(')
+        elif lazer[i] == ')':
+            if lazer[i-1] == '(':
+                continue
+            else:
+                stack.pop()
+
+    print(f'#{tc} {seg}')
