36b441b0bb9e7b099530430422579cf9fae26d1f
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-14 17:15:25 +0900

    [Silver II] Title: 쇠막대기, Time: 72 ms, Memory: 32472 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,18 @@
+lazer = list(input())
+stack = []
+# 잘린 조각
+seg = 0
+for i in range(len(lazer)):
+    if lazer[i] == '(':
+        if lazer[i+1] == ')':
+            seg += len(stack)
+        else:
+            seg += 1
+            stack.append('(')
+    elif lazer[i] == ')':
+        if lazer[i-1] == '(':
+            continue
+        else:
+            stack.pop()
+
+print(seg)
