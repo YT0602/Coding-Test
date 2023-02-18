056549de661234caf8deb6983bcbf37753eff720
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-18 23:26:27 +0900

    [Silver II] Title: 최대 힙, Time: 124 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+h = []
+cnt = 0
+for i in range(N):
+    num = int(input())
+    if num == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(h))
+    else:
+        heapq.heappush(h, -num)
