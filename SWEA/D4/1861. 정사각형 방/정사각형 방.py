a93f607247430382eabfc59aefb3a9bc5de4ee1f
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-22 16:19:25 +0900

    [D4] Title: 정사각형 방, Time: 297 ms, Memory: 66,492 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -0,0 +1,33 @@
+def DFS(r, c):
+    global cnt
+    for k in range(4):
+        nx = r + dx[k]
+        ny = c + dy[k]
+        if 0 <= nx < N and 0 <= ny < N:
+            if room[nx][ny] - room[r][c] == 1:
+                cnt += 1
+                visited[nx][ny] = 1
+                DFS(nx, ny)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    room = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    mx_cnt = 1
+    room_num = N**2
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0:
+                cnt = 1
+                DFS(i, j)
+                if cnt > mx_cnt:
+                    mx_cnt = cnt
+                    room_num = room[i][j]
+                elif cnt == mx_cnt:
+                    if room_num > room[i][j]:
+                        room_num = room[i][j]
+    print(f'#{tc} {room_num} {mx_cnt}')
