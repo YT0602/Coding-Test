19c816f8659d31241c7dc773f32493d6949ba3ae
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-02-28 17:01:41 +0900

    [Gold V] Title: LCS, Time: 208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -4,7 +4,8 @@ input = sys.stdin.readline
 s1 = input().strip()
 s2 = input().strip()
 dp = [0] * 1000
-
+# dp 각 문자별로 다른 문자열을 순회하면서
+# 같은 문자면 + 1, 다른 경우 누적값이 해당 위치 dp보다 작으면 갱신
 for i in range(len(s1)):
     mx = 0
     for j in range(len(s2)):
