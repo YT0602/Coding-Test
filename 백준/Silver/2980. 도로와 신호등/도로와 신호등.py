27d20073ecf506f37f668ce4da96bf4c6c32e5fa
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-03-02 17:19:59 +0900

    [Silver IV] Title: 도로와 신호등, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2980. 도로와 신호등/도로와 신호등.py
@@ -0,0 +1,21 @@
+N, L = map(int, input().split())
+t = 0
+now = 0
+
+for i in range(N):
+    D, R, G = map(int, input().split())
+    # 다음 신호등까지 시간
+    next = t + D - now
+    # 나머지가 R 보다 작으면 대기해야됨
+    if next % (R+G) == 0:
+        t += R+D-now
+        now = D
+    elif next % (R+G) < R:
+        t += (R - (next % (R+G))) + D-now
+        now = D
+    elif next % (R+G) >= R:
+        t += D-now
+        now = D
+
+t += L-now
+print(t)
