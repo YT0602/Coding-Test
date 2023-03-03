5e8bd2578a595c796b419cb338ab291eacaf5a95
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-03-03 10:32:50 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 463 ms, Memory: 64,912 KB -BaekjoonHub

---
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -0,0 +1,21 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M, K = map(int, input().split())
+    come = list(map(int, input().split()))
+    come.sort()
+    # 도착시간 간격 리스트
+    come_new = [come[0]]
+    for i in range(1, N):
+        come_new.append(come[i]-come[i-1])
+    make = 0
+    ans = 'Possible'
+    t = 0
+    for i in range(N):
+        make += ((come_new[i] + t) // M)*K
+        t = (come_new[i] + t) % M
+        if make == 0 and come_new[i] < M:
+            ans = 'Impossible'
+            break
+        else:
+            make -= 1
+    print(f'#{tc} {ans}')
