35b545ba9626e03476463bcd8caba0a6482238d0
최영태 <dudxo7721@naver.com>
2023-03-16 13:46:13 +0900

    풀이완료

--- 연습장/1918 후위표기식.py
+++ 연습장/1918 후위표기식.py
@@ -3,19 +3,26 @@ solve = input()
 calcul = []
 
 for i in solve:
+    # 알파벳이면 그대로 출력
     if i.isalpha():
         print(i, end='')
     else:
+        # 여는 괄호면 push
         if i == '(':
             calcul.append(i)
+        # 닫는 괄호면 여는괄호 나올때 까지 pop해서 출력
         elif i == ')':
             while len(calcul) != 0 and calcul[-1] != '(':
                 print(calcul.pop(), end='')
+            # 여는괄호 제거
             calcul.pop()
+        # 곱셈 나눗셈이면
         elif i == '*' or i == '/':
             if calcul:
+                # top이 우선순위가 더 낮으면 push
                 if calcul[-1] == '(' or calcul[-1] == '+' or calcul[-1] == '-':
                     calcul.append(i)
+                # 우선순위가 같거나 높으면 낮은거 나올때까지 pop해서 출력하고 push
                 else:
                     while calcul[-1] == '*' or calcul[-1] == '/':
                         print(calcul.pop(), end='')
@@ -34,5 +41,6 @@ for i in solve:
                     calcul.append(i)
             else:
                 calcul.append(i)
+# 남은거 출력
 while calcul:
     print(calcul.pop(), end='')
