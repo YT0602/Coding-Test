a21f155a7f5024b1d680b9c0f8419b719a9d7c19
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-03-26 18:04:08 +0900

    [Gold IV] Title: 테트로미노, Time: 180 ms, Memory: 37992 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -2,45 +2,47 @@ import sys
 
 input = sys.stdin.readline
 
+
+def DFS(r, c, idx, cnt):
+    global mx_cnt
+    # 남은 칸들이 배열의 최댓값이더라도 최댓값보다 작으면 탐색 종료
+    if mx_cnt >= cnt + max_val * (3 - idx):
+        return
+    # 깊이 3까지 왔다면 최댓값 갱신
+    if idx == 3:
+        mx_cnt = max(mx_cnt, cnt)
+        return
+    else:
+        for i in range(4):
+            nr = r + dx[i]
+            nc = c + dy[i]
+            if 0 <= nr < N and 0 <= nc < M:
+                if visit[nr][nc] == 0:
+                    # ㅗ 모양 탐색
+                    if idx == 1:
+                        visit[nr][nc] = 1
+                        DFS(r, c, idx + 1, cnt + arr[nr][nc])
+                        visit[nr][nc] = 0
+                    # 다른 모양 탐색
+                    visit[nr][nc] = 1
+                    DFS(nr, nc, idx + 1, cnt + arr[nr][nc])
+                    visit[nr][nc] = 0
+
+
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-# 모든 조각의 경우의 수
-tetris = [[(0, 0), (0, 1), (0, 2), (0, 3)],
-          [(0, 0), (1, 0), (2, 0), (3, 0)],
-          [(0, 0), (1, 0), (0, 1), (1, 1)],
-          [(0, 0), (1, 0), (2, 0), (2, 1)],
-          [(0, 1), (1, 1), (2, 1), (2, 0)],
-          [(0, 0), (0, 1), (1, 1), (2, 1)],
-          [(0, 0), (0, 1), (1, 0), (2, 0)],
-          [(0, 0), (1, 0), (1, 1), (1, 2)],
-          [(0, 2), (1, 1), (1, 2), (1, 0)],
-          [(0, 0), (0, 1), (0, 2), (1, 2)],
-          [(0, 0), (1, 0), (0, 1), (0, 2)],
-          [(0, 0), (1, 0), (1, 1), (2, 1)],
-          [(0, 1), (1, 1), (1, 0), (2, 0)],
-          [(1, 0), (1, 1), (0, 1), (0, 2)],
-          [(0, 0), (0, 1), (1, 1), (1, 2)],
-          [(0, 1), (1, 0), (1, 1), (1, 2)],
-          [(0, 0), (0, 1), (0, 2), (1, 1)],
-          [(0, 0), (1, 0), (1, 1), (2, 0)],
-          [(0, 1), (1, 1), (1, 0), (2, 1)]]
-
+visit = [([0] * M) for _ in range(N)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 mx_cnt = 0
-for i in range(N):
-    for j in range(M):
-        # 각 칸마다 테트리스 종류별로 대입
-        for k in tetris:
-            # 종류마다 카운트 초기화
-            cnt = 0
-            for l in range(4):
-                nx = i + k[l][0]
-                ny = j + k[l][1]
-                if 0 <= nx < N and 0 <= ny < M:
-                    cnt += arr[nx][ny]
-                # 배역 밖으로 나가면 종료
-                else:
-                    break
-            # 4칸 모두 탐색 후에 최댓값 갱신
-            mx_cnt = max(mx_cnt, cnt)
-# print(arr)
+# 배열 안의 값 중 최댓값
+max_val = max(map(max, arr))
+
+for r in range(N):
+    for c in range(M):
+        # 탐색 후 방문처리 해제
+        visit[r][c] = 1
+        DFS(r, c, 0, arr[r][c])
+        visit[r][c] = 0
+
 print(mx_cnt)
