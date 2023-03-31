b3bfd54702d5c856f8f02d33d3aaf4d5a1c11753
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-03-31 10:00:39 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 290 ms, Memory: 63,572 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,24 @@
+def tower(num, sm):
+    global cnt, mn
+    if sm > mn:
+        return
+    # N까지 왔을때 B이상이면 최소 비교
+    if num == N:
+        if sm >= B:
+            mn = min(sm, mn)
+        return
+
+    # num번째 수를 포함하는 경우
+    tower(num+1, sm+H[num])
+    # 포함하지 않는 경우
+    tower(num+1, sm)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N, B = map(int, input().split())
+    H = list(map(int, input().split()))
+    cnt = 0
+    mn = 200000
+    tower(0, 0)
+    print(f'#{tc} {mn-B}')
