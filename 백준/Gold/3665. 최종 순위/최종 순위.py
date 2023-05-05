36ce230882f4cad0df106c6a901f95cd40c1225f
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-05-06 00:57:40 +0900

    Update 최종 순위.py

--- 백준/Gold/3665. 최종 순위/최종 순위.py
+++ 백준/Gold/3665. 최종 순위/최종 순위.py
@@ -1,68 +1,59 @@
 from collections import deque
-import sys
 
-t = int(sys.stdin.readline())
+tc = int(input())
+for _ in range(tc):
+    N = int(input())
 
-for i in range(t):
-    n = int(sys.stdin.readline())
+    graph = [[] for _ in range(N+1)]
+    in_node = [0] * (N+1)
+    q = deque()
+    new_rank = []
 
-    graph = [[] for _ in range(n + 1)]
-    inDegree = [0 for _ in range(n + 1)]
-    queue = deque()
-    answer = []
-    flag = 0
+    rank = list(map(int, input().split()))
+    for i in range(N-1):
+        for j in range(i+1, N):
+            graph[rank[i]].append(rank[j])
+            in_node[rank[j]] += 1
 
-    team = list(map(int, sys.stdin.readline().rstrip().split()))
-
-    for j in range(n - 1):
-        for k in range(j + 1, n):
-            graph[team[j]].append(team[k])
-            inDegree[team[k]] += 1
-
-    m = int(sys.stdin.readline())
-    for j in range(m):
-        first, second = map(int, sys.stdin.readline().rstrip().split())
-        flag = True
-
-        for k in graph[first]:
-            if k == second:
-                graph[first].remove(second)
-                inDegree[second] -= 1
-                graph[second].append(first)
-                inDegree[first] += 1
-                flag = False
-
-        if flag:
+    M = int(input())
+    for _ in range(M):
+        first, second = map(int, input().split())
+        if first in graph[second]:
+            # 간선 제거하고 진입차수 -1
             graph[second].remove(first)
-            inDegree[first] -= 1
+            in_node[first] -= 1
+            # 간선 새로 추가하고 진입차수 + 1
             graph[first].append(second)
-            inDegree[second] += 1
+            in_node[second] += 1
+        else:
+            graph[first].remove(second)
+            in_node[second] -= 1
+            graph[second].append(first)
+            in_node[first] += 1
 
-    for j in range(1, n + 1):
-        if inDegree[j] == 0:
-            queue.append(j)
+    # 진입차수 0인 노드 큐에 삽입
+    for i in range(1, N+1):
+        if in_node[i] == 0:
+            q.append(i)
 
-    if not queue:
-        print("IMPOSSIBLE")
-        continue
-
-    result = True
-    while queue:
-        if len(queue) > 1:
-            result = False
+    ans = True
+    while q:
+        # 처음에 진입차수가 0인 노드가 2개이상 있으면 불가능
+        if len(q) > 1:
+            ans = False
             break
 
-        tmp = queue.popleft()
-        answer.append(tmp)
-        for j in graph[tmp]:
-            inDegree[j] -= 1
-            if inDegree[j] == 0:
-                queue.append(j)
-            elif inDegree[j] < 0:
-                result = False
+        cur = q.popleft()
+        new_rank.append(cur)
+        for j in graph[cur]:
+            in_node[j] -= 1
+            if in_node[j] == 0:
+                q.append(j)
+            elif in_node[j] < 0:
+                ans = False
                 break
-
-    if not result or len(answer) < n:
+    if not ans or len(new_rank) < N:
         print("IMPOSSIBLE")
     else:
-        print(*answer)
+        print(*new_rank)
+    # print(graph)
