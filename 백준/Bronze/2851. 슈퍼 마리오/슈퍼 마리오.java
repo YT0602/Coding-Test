862fc9b88e021b0b0cb9b39a383fcf0b1601148c
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-07-31 04:00:42 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int s = 0;
+		int res=0;
+		for(int i=0;i<10;i++) {
+			int a = Integer.parseInt(br.readLine());
+			if(s<100){
+				s+=a;
+				if(Math.abs(res-100)>Math.abs(s-100)) {
+					res = s;
+				}else if(Math.abs(res-100)==Math.abs(s-100)) {
+                    if (res > s){
+                        res = res;
+                    } else{
+                        res = s;
+                    }
+				}
+			}
+		}
+		System.out.println(res);
+	}
+}
