1bbfd6768c6c57346703e8a6c85af8e9cead8fb7
Youngtae_Choi <115714519+YT0602@users.noreply.github.com>
2023-08-26 17:22:55 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 163 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,16 +1,41 @@
-def inorder(n):
-    if n > N:
-        return
-    elif n <= N:
-        inorder(2*n)
-        print(nodes[n-1][1], end='')
-        inorder(2*n+1)
 
+import java.util.*;
+import java.io.*;
 
-for tc in range(1, 11):
-    N = int(input())
-    nodes = [list(input().split()) for _ in range(N)]
-    print(f'#{tc}', end=' ')
-    inorder(1)
-
-    print()
+class Solution
+{
+	static char[] arr;
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			sb.append("#" + tc + " ");
+			N = Integer.parseInt(br.readLine());
+			// 방문 노드 알파벳 저장 배열
+			arr = new char[N+1];
+			
+			for (int i=1; i <= N; i++) {
+				// 입력 공백으로 나누고 노드번호별 알파벳 저장
+				arr[i] = br.readLine().split(" ")[1].charAt(0);
+			}
+			// 중위순회
+			DFS(1);
+			sb.append("\n");
+			
+		}
+		System.out.println(sb);
+	}
+	public static void DFS(int num) {
+		// 트리 밖으로 나가면 리턴
+		if (num > N) return;
+		// 왼쪽 자식부터 탐색
+		DFS(num * 2);
+		// 현재노드 출력
+		sb.append(arr[num]);
+		// 오른쪽 자식
+		DFS(num * 2 + 1);
+	}
+}
